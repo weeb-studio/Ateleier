5924ed3879e06f61c66dd90136f30f787e96ef4c
Patrick23718 <noukimi.patrick@gmail.com>
2021-10-05 17:53:13 +0100

    05/10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  SERVER_URL: 'http://localhost:8080',
+  SERVER_URL: 'http://93.90.207.75:3000', //'http://localhost:8080',
   production: false,
 };
 
