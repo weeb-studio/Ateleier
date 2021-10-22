a70dd355f0b57d1f5cf316e6d6f3ac09cffd3b82
sikamloïque <loicsikam272@gmail.com>
2021-10-22 09:58:00 +0200

    mise a jour du 21/10

--- src/app/pages/client/dash-cliente/dash-cliente.component.ts
+++ src/app/pages/client/dash-cliente/dash-cliente.component.ts
@@ -7,7 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashClienteComponent implements OnInit {
 
-  constructor() { }
+    page1 : boolean = true;
+    page2 : boolean = false;
+    page3 : boolean = false;
+    Mawishlist : boolean = true;
+  recommendation : boolean = false;
+
+  showMawishlist(){
+    this.Mawishlist = true;
+    this.recommendation = false;
+  }
+  showRecommendation(){
+    this.recommendation = !this.recommendation;
+    this.Mawishlist = !this.Mawishlist;
+  }
+  items : any =[
+    {
+      img : 'assets/prod1.svg',
+      prix : '12,00€'
+    },
+    {
+      img : 'assets/prod2.svg',
+      prix : '120,00€'
+    },
+    {
+      img : 'assets/prod3.svg',
+      prix : '55,00€'
+    },
+    {
+      img : 'assets/prod1.svg',
+      prix : '145,00€'
+    },
+    {
+      img : 'assets/prod3.svg',
+      prix : '8,00€'
+    }
+  ];
+  constructor () { }
 
   ngOnInit(): void {
   }
