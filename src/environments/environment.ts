7bf951600e88c804c8b27fa429132324c4bb3e77
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2022-05-17 17:57:02 +0100

    Résolution du problème du chemin de l'api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  SERVER_URL: 'https/atelier-api.bigoodee.com',
+  SERVER_URL: 'https://atelier-api.bigoodee.com',
   production: false,
 };
 
